cbe7776cf3bfb43addbf7e084e9ba7fabfe6dd0e
MimiMicek <makaveli_girl1996@hotmail.com>
2020-05-30 22:37:57 +0200

    calendar add booking done

---
+++ apis/api-add-booking.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once __DIR__.'/../db-connect.php';
+session_start();
+
+if (!isset($_SESSION['userId'])) {
+    header('Location: login.php');
+}
+
+$ownerUserId = $_SESSION['userId'];
+if(empty($endDate)){ sendResponse(0, __LINE__, "Owner userId is missing!"); }
+
+$sitterUserId = $_POST['sitterUserId'] ?? '';
+if(empty($endDate)){ sendResponse(0, __LINE__, "Sitter userId is missing!"); }
+
+//TODO clean the number from the dot . before saving
+
+$bookingTypeId = $_POST['typeOption'] ?? '';
+if(empty($bookingTypeId)){ sendResponse(0, __LINE__, "Please enter booking type!"); }
+
+$startDate = $_POST['startDate'] ?? '';
+if(empty($startDate)){ sendResponse(0, __LINE__, "Start date is missing!"); }
+
+$endDate = $_POST['endDate'] ?? '';
+if(empty($endDate)){ sendResponse(0, __LINE__, "End date is missing!"); }
+
+try {
+
+    //Saving the booking
+    $stmt = $db->prepare('INSERT INTO bookings VALUES( null, :ownerUserId, :sitterUserId, :startDate, :endDate, :bookingTypeId)');
+    $stmt->bindValue(':ownerUserId', $ownerUserId );
+    $stmt->bindValue(':sitterUserId', $sitterUserId );
+    $stmt->bindValue(':bookingTypeId', $bookingTypeId );
+    $stmt->bindValue(':startDate', $startDate );
+    $stmt->bindValue(':endDate', $endDate );
+    $stmt->execute();
+
+    var_dump($stmt->rowCount());
+
+    //Using rowcount() when INSERTing, UPDATEing or DELETEing
+    if( $stmt->rowCount() == 0 ){
+        echo 'Sorry, the booking was not saved!';
+        exit;
+    }
+
+}catch( PDOEXception $ex ){
+    echo $ex;
+}
+
+header("refresh:5;url=../my-profile");
+sendResponse(1, __LINE__, "Your booking is saved!");
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function sendResponse($bStatus, $iLineNumber, $sMessage){
+    echo '{"status":'.$bStatus.', "code":'.$iLineNumber.', "message":'.$sMessage.'}';
+    exit;
+}
--- book-sitter.php
+++ book-sitter.php
@@ -1,34 +1,154 @@
 <?php
 
-
 require_once 'header.php';
 require_once __DIR__ . '/db-connect.php';
 
-//ini_set('display_errors', 0);
+ini_set('display_errors', 0);
 session_start();
 
 if (!isset($_SESSION['userId'])) {
     header('Location: login.php');
 }
 
-$userId = $_SESSION['userId'];
+$ownerUserId = $_SESSION['userId'];
+$sitterUserId = $_GET['id'];
 
-try {
-    //Getting users details
-    $stmt = $db->prepare('SELECT *
-                                   FROM users
-                                   WHERE users.user_type_id_fk = :typeSitter');
+//var_dump($sitterUserId);
 
-    $stmt->bindValue(':typeSitter', $typeSitter);
+?>
 
-    $stmt->execute();
+<head>
+    <link rel="stylesheet" href="fullcalendar/fullcalendar.min.css" />
+    <script src='http://fullcalendar.io/js/fullcalendar-2.1.1/lib/moment.min.js'></script>
+    <script src='http://fullcalendar.io/js/fullcalendar-2.1.1/lib/jquery.min.js'></script>
+    <script src="http://fullcalendar.io/js/fullcalendar-2.1.1/lib/jquery-ui.custom.min.js"></script>
+    <script src='http://fullcalendar.io/js/fullcalendar-2.1.1/fullcalendar.min.js'></script>
 
-    $aRows = $stmt->fetchAll();
+    <script>
+        $(document).ready(function () {
+            let calendar = $('#calendar').fullCalendar({
+                editable: true,
+                events: 'apis/api-get-bookings',
+                displayEventTime: false,
+                eventRender: function (event, element, view) {
+                    if (event.allDay === 'true') {
+                        event.allDay = true;
+                    } else {
+                        event.allDay = false;
+                    }
+                },
+                selectable: true,
+                selectHelper: true,
+                select: function (start, end, allDay) {
+                    let typeOption = prompt('Please enter a number of the booking type\n' +
+                                            '1. Walk (1 day)\n' +
+                                            '2. Overnight stay (2 days) or\n' +
+                                            '3. Vacation (3 or more)');
 
-    if (count($aRows) == 0) {
-        echo 'Sorry no users found!';
-    }
+                    if (typeOption) {
+                        let startDate = moment(start, 'DD.MM.YYYY').format('YYYY-MM-DD');
+                        let endDate = moment(end, 'DD.MM.YYYY').format('YYYY-MM-DD');
+                        let sitterUserId = "<?php echo $sitterUserId;?>";
 
-} catch (PDOException $ex) {
-    echo $ex;
-}
+                        console.log(sitterUserId);
+
+                        $.ajax({
+                            url: 'apis/api-add-booking',
+                            data: 'sitterUserId='+ sitterUserId +
+                                  '&typeOption=' + typeOption +
+                                  '&startDate=' + startDate +
+                                  '&endDate=' + endDate,
+                            type: 'POST',
+                            success: function (data) {
+                                displayMessage('Booked Successfully');
+                            }
+                        });
+                        calendar.fullCalendar('renderEvent',
+                            {
+                                title: typeOption,
+                                start: startDate,
+                                end: endDate,
+                                allDay: allDay
+                            },
+                            true
+                        );
+                    }
+                    calendar.fullCalendar('unselect');
+                }
+
+
+               /* eventDrop: function (event, delta) {
+                    var start = $.fullCalendar.formatDate(event.start, "YYYY-MM-DD");
+                    var end = $.fullCalendar.formatDate(event.end, "YYYY-MM-DD");
+                    $.ajax({
+                        url: 'edit-event.php',
+                        data: 'title=' + event.title + '&start=' + start + '&end=' + end + '&id=' + event.id,
+                        type: "POST",
+                        success: function (response) {
+                            displayMessage("Updated Successfully");
+                        }
+                    });
+                },
+                eventClick: function (event) {
+                    var deleteMsg = confirm("Do you really want to delete?");
+                    if (deleteMsg) {
+                        $.ajax({
+                            type: "POST",
+                            url: "delete-event.php",
+                            data: "&id=" + event.id,
+                            success: function (response) {
+                                if(parseInt(response) > 0) {
+                                    $('#calendar').fullCalendar('removeEvents', event.id);
+                                    displayMessage("Deleted Successfully");
+                                }
+                            }
+                        });
+                    }
+                }*/
+
+            });
+        });
+
+        function displayMessage(message) {
+            $(".response").html("<div class='success'>"+message+"</div>");
+            setInterval(function() { $(".success").fadeOut(); }, 1000);
+        }
+    </script>
+
+    <style>
+
+        .response {
+            height: 60px;
+        }
+
+        .success {
+            background: #cdf3cd;
+            padding: 10px 60px;
+            border: #c3e6c3 1px solid;
+            display: inline-block;
+        }
+
+        .fc-content{
+            background: tomato;
+        }
+
+        .fc-event{
+            border: unset;
+            font-size: medium;
+        }
+    </style>
+</head>
+<body>
+    <h2 class="pt-3">Book a dog sitter</h2>
+
+    <div id="calendar"></div>
+    <div class="response"></div>
+</body>
+
+
+
+<?php
+
+require_once 'footer.php';
+
+?>
