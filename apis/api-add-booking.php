6e0b4945608b6e37fa332cf8d1d67c277cdec57f
MimiMicek <makaveli_girl1996@hotmail.com>
2020-06-01 15:03:29 +0200

    calendar removed and booking form done

--- apis/api-add-booking.php
+++ apis/api-add-booking.php
@@ -7,16 +7,17 @@ if (!isset($_SESSION['userId'])) {
     header('Location: login.php');
 }
 
+$aTypeIds = [1, 2, 3];
+
 $ownerUserId = $_SESSION['userId'];
-if(empty($endDate)){ sendResponse(0, __LINE__, "Owner userId is missing!"); }
+if(empty($ownerUserId)){ sendResponse(0, __LINE__, "Owner userId is missing!"); }
 
 $sitterUserId = $_POST['sitterUserId'] ?? '';
-if(empty($endDate)){ sendResponse(0, __LINE__, "Sitter userId is missing!"); }
+if(empty($sitterUserId)){ sendResponse(0, __LINE__, "Sitter userId is missing!"); }
 
-//TODO clean the number from the dot . before saving
-
-$bookingTypeId = $_POST['typeOption'] ?? '';
+$bookingTypeId = (int)($_POST['typeOption']) ?? '';
 if(empty($bookingTypeId)){ sendResponse(0, __LINE__, "Please enter booking type!"); }
+if(!in_array($bookingTypeId, $aTypeIds)){ sendResponse(0, __LINE__, "Please a booking type number => 1, 2 or 3!"); }
 
 $startDate = $_POST['startDate'] ?? '';
 if(empty($startDate)){ sendResponse(0, __LINE__, "Start date is missing!"); }
@@ -25,6 +26,24 @@ $endDate = $_POST['endDate'] ?? '';
 if(empty($endDate)){ sendResponse(0, __LINE__, "End date is missing!"); }
 
 try {
+
+    $stmt = $db->prepare('SELECT *
+                                   FROM bookings AS b
+                                   WHERE b.start_date = :startDate
+                                   OR b.end_date = :endDate');
+
+    $stmt->bindValue(':startDate', $startDate );
+    $stmt->bindValue(':endDate', $endDate );
+
+    $stmt->execute();
+
+    $aRows = $stmt->fetchAll();
+
+    if(count($aRows) > 0){
+        echo 'Sorry this dates are already occupied. Please try again!';
+        exit;
+        header("refresh:5;url=../find-sitter");
+    }
 
     //Saving the booking
     $stmt = $db->prepare('INSERT INTO bookings VALUES( null, :ownerUserId, :sitterUserId, :startDate, :endDate, :bookingTypeId)');
